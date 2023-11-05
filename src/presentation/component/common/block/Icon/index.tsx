52d1245187268a11532878bd1b79c342c3e7dd8b
Vladislav Dietrich <theithelor@gmail.com>
2023-11-05 13:26:18 +1000

    feat: switch from react-icons to phosphor-icons

--- src/presentation/component/common/block/Icon/index.tsx
+++ src/presentation/component/common/block/Icon/index.tsx
@@ -1,19 +1,22 @@
 import { FC } from 'react';
+import { IconProps, Icon as IconType } from '@phosphor-icons/react';
 import clsx from 'clsx';
-import { IconBaseProps, IconType } from 'react-icons';
 
-type IconPropsT = IconBaseProps &
+type PropsT = IconProps &
     ClassName & {
         icon: IconType;
     };
 
-const Icon: FC<IconPropsT> = (props) => {
+const Icon: FC<PropsT> = (props) => {
     const { icon: IconComponent, className, ...restProps } = props;
 
     return (
         <IconComponent
             size={24}
-            className={clsx('transition-fill duration-fast', className)}
+            className={clsx(
+                'transition duration-fast [&_path[opacity="0.2"]]:opacity-10',
+                className,
+            )}
             {...restProps}
         />
     );
